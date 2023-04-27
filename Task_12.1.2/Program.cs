f986f26bb231e53eb6567061a287e4067d7e576a
Алексей <setgecko@gmail.com>
2023-04-27 04:09:49 +0300

    Добавьте файлы проекта.

---
+++ Task_12.1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_12._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            string greetings = "Привет, " + name;
+            Console.WriteLine(greetings);
+        }
+    }
+}
