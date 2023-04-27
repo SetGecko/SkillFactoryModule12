91978476535621a9e269c7cc0c5971bf62a3d417
Алексей <setgecko@gmail.com>
2023-04-27 18:56:45 +0300

    Выполнил задание 12.3.3

---
+++ Task_12.3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_12._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int GetIndex(int[] array, int element)
+        {
+            int index;
+            for (index = 0; index < array.Length; index++)
+            {
+                if (array[index] > element)
+                    return index;
+            }
+
+            return index;
+        }
+    }
+}
