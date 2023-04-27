6f7a544d08ab26f0adc9aab2a935c41e8803bbde
Алексей <setgecko@gmail.com>
2023-04-27 04:18:25 +0300

    Выполнил задание 12.1.3

---
+++ Task_12.1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_12._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            int count = Convert.ToInt32(Console.ReadLine());
+            var array = new int[count];
+            for(int i = 0; i < count; i++)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Все элементы записаны");
+        }
+    }
+}
