9da07d43f54083a658f454c9a2b7c12a04a52a8d
Алексей <setgecko@gmail.com>
2023-04-27 18:51:51 +0300

    Выполнил задание 12.3.1

---
+++ Task_12.3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_12._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int BinarySearch(int value, int[] array, int left, int right)
+        {
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+
+                var midElement = array[middle];
+
+                if (midElement == value)
+                {
+                    return middle;
+                }
+                else if (value < midElement)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
