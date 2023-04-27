a0fd3fa8eaadd8acab48cd934e4ba86453ab6814
Алексей <setgecko@gmail.com>
2023-04-27 19:50:12 +0300

    Практика 12.4.2

---
+++ Task_12.4.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Task_12._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UseString(70000);
+            Console.WriteLine("Ready to switch");
+            Console.ReadKey();
+
+            UseStringBuilder(70000);
+            Console.ReadKey();
+        }
+        static string UseString(int n)
+        {
+            string value = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                value += i.ToString();
+                value += " ";
+            }
+
+            return value;
+        }
+
+        static string UseStringBuilder(int n)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                builder.Append(i.ToString());
+                builder.Append(" ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
