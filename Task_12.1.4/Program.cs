11ac27c1403da157dab78b47a295cfaaa8b0e171
Алексей <setgecko@gmail.com>
2023-04-27 04:26:19 +0300

    Выполнил задание 12.1.4 в варианте ifElse и switchCase

---
+++ Task_12.1.4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_12._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой возраст");
+            var age = Convert.ToInt32(Console.ReadLine());
+            //if (age < 13)
+            //{
+            //    Console.WriteLine("Пользователи младше 14 лет не могут быть зарегестрированы");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Вы успешно зарегестрированы");
+            //}
+            switch (age)
+            {
+                case < 13:
+                    Console.WriteLine("Пользователи младше 14 лет не могут быть зарегестрированы");
+                    break;
+                case >= 14:
+                    Console.WriteLine("Вы успешно зарегестрированы");
+                    break;
+            }
+        }
+    }
+}
