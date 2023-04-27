cbb1ca4e9e8e44a9beeb09669a3bb1cf4e372e3b
Алексей <setgecko@gmail.com>
2023-04-27 06:25:40 +0300

    тест

---
+++ testSql/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace testSql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-82VM5QH\\SQLEXPRESS"; //.database.windows.net
+                builder.UserID = "NT Service\\MSSQL$SQLEXPRESS";
+                builder.Password = "123456";
+                builder.IntegratedSecurity = false;
+                builder.TrustServerCertificate = true;
+                builder.InitialCatalog = "Test";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
